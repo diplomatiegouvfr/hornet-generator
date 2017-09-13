2bac30fce93084939ecd745bff064c421f6f3cb0
Muriel cadiot <muriel.cadiot@diplomatie.gouv.fr>
2017-09-13 14:24:00 +0000

    modif console
    git-svn-id: https://forge.dsinet.diplomatie.gouv.fr/svn/Hornet.js/hornet-generator/trunk@211491 5d00b825-b27f-43c0-b460-8f767e7a2fcd

--- fr.gouv.diplomatie.papyrus.codegen.sql/src/fr/gouv/diplomatie/papyrus/codegen/sql/generators/PackageGenerator.java
+++ fr.gouv.diplomatie.papyrus.codegen.sql/src/fr/gouv/diplomatie/papyrus/codegen/sql/generators/PackageGenerator.java
@@ -4,15 +4,15 @@ import org.eclipse.papyrus.infra.tools.file.IPFileSystemAccess;
 import org.eclipse.uml2.uml.Package;
 
 import fr.gouv.diplomatie.papyrus.codegen.core.generators.GeneratorUtils;
+import fr.gouv.diplomatie.papyrus.codegen.core.utils.Utils;
 import fr.gouv.diplomatie.papyrus.codegen.sql.xtend.pakkage.PackageDatabaseScriptGenerator;
 
 public class PackageGenerator{
 
-	
 	public static void generateDatabaseScript(Package pakkage, IPFileSystemAccess fileSystemAccess) {
 		String fileName = GeneratorUtils.getDatabaseScriptPath(pakkage, false) + ".sql";
 		
-		GeneratorUtils.out.println("PackageGenerator.generateDatabaseScript : "  + pakkage.getName() + ", fichier : " + fileName);
+		Utils.console.out.println("PackageGenerator.generateDatabaseScript : "  + pakkage.getName() + ", fichier : " + fileName);
 		
 		fileSystemAccess.generateFile(fileName, PackageDatabaseScriptGenerator.generateCode(pakkage).toString());
 	}
